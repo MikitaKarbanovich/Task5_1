ca51b2c23c8d490462f98fd79057b17dbdcfee39
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-09-27 13:11:11 +0300

    Add project files.

---
+++ Task5_1/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_1
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return $"Points is: {X} and {Y}";
+        } 
+        public override bool Equals(System.Object obj)
+        {  
+                       if (obj == null)
+            {
+                return false;
+            }
+
+            
+            Point p = obj as Point;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+
+            return (X == p.X) && (Y == p.Y);
+        }
+    }
+}
---
+++ Task5_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.X = 2;
+            point.Y = 3;
+            Console.WriteLine(point.Equals(point));
+        }
+    }
+}
